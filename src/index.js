f3dbe76ed409b34628145d2821bd985b55a58e0a
iftype <qwes3123@gmai.com>
2024-12-08 13:40:02 +0900

    build

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 const rootNode = document.getElementById("root");
 
 ReactDOM.createRoot(rootNode).render(
-  <App />
-  // </React.StrictMode>
-  // <React.StrictMode>
+    <App />
+    // </React.StrictMode>
+    // <React.StrictMode>
 );
